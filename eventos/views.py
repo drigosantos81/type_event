094a3ba9c379145f3059c6a945598f30d653a5ce
Rodrigo Emanuel <drigosantos@gmail.com>
2023-04-16 20:46:32 -0300

    Adicionado navegacao completa, certificados e consulta dos certificados

--- eventos/views.py
+++ eventos/views.py
@@ -1,15 +1,19 @@
 import os
 import csv
+import sys
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.http import Http404
-from .models import Evento
+from .models import Evento, Certificado
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.http import HttpResponse
 from secrets import token_urlsafe
 from django.conf import settings
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # Create your views here.
 @login_required
@@ -87,7 +91,7 @@ def participantes_evento(request, id):
 		return render(request, 'participantes_evento.html', {'participantes':participantes, 'evento':evento})
 	
 def gerar_csv(request, id):
-	evento = get_object_or_404(Evento, id=id)	
+	evento = get_object_or_404(Evento, id=id)
 	if not evento.criador == request.user:
 		raise Http404('Esse evento não é seu')
 	
@@ -103,3 +107,81 @@ def gerar_csv(request, id):
 			writer.writerow(x)
 	
 	return redirect(f'/media/{token}')
+
+def certificados_evento(request, id):
+	evento = get_object_or_404(Evento, id=id)
+	if not evento.criador == request.user:
+		raise Http404('Esse evento não é seu')
+	
+	if request.method == "GET":
+		qtd_certificados = evento.participantes.all().count() - Certificado.objects.filter(evento=evento).count()
+		return render(request, 'certificados_evento.html', {'qtd_certificados': qtd_certificados, 'evento': evento})
+	
+def gerar_certificado(request, id):
+	evento = get_object_or_404(Evento, id=id)
+	if not evento.criador == request.user:
+		raise Http404('Esse evento não é seu')
+
+	path_template = os.path.join(settings.BASE_DIR, 'templates/static/evento/img/template_certificado.png')
+	path_fonte = os.path.join(settings.BASE_DIR, 'templates/static/evento/fontes/arimo.ttf')
+
+	for participante in evento.participantes.all():
+		# TODO: Validar se o certificado já foi gerado
+		img = Image.open(path_template)
+		draw = ImageDraw.Draw(img)
+		
+		# fonte_nome = ImageFont.truetype(path_fonte, 80)
+		# fonte_info = ImageFont.truetype(path_fonte, 30)
+
+		fonte_nome = ImageFont.load_default()
+		fonte_info = ImageFont.load_default()
+
+# Faz assim, para contornar esse erro
+# no font_nome =
+# e no font_ifo =
+# tira o ImageFonte.truetype
+# e coloca assim nos dois
+# ImageFont.load_default()
+
+		draw.text((230, 651), f'{participante.username}', font=fonte_nome, fill=(0,0,0))
+		draw.text((761, 782), f'{evento.nome}', font=fonte_info, fill=(0,0,0))
+		draw.text((816, 849), f'{evento.carga_horaria}', font=fonte_info, fill=(0,0,0))
+
+		output = BytesIO()
+		img.save(output, format="png", quality=100)
+		output.seek(0)
+
+		img_final = InMemoryUploadedFile(
+			output,
+			'ImageField',
+			f'{token_urlsafe(8)}.png',
+			'image/jpeg',
+			sys.getsizeof(output),
+			None
+		)
+		
+		certificado_gerado = Certificado(
+			certificado=img_final,
+			participante=participante,
+			evento=evento,
+		)
+		
+		certificado_gerado.save()
+
+	messages.add_message(request, constants.SUCCESS, 'Certificados gerados')
+	
+	return redirect(reverse('certificados_evento', kwargs={'id': evento.id}))
+
+def procurar_certificado(request, id):
+	evento = get_object_or_404(Evento, id=id)
+	if not evento.criador == request.user:
+		raise Http404('Esse evento não é seu')
+	
+	email = request.POST.get('email')
+	certificado = Certificado.objects.filter(evento=evento).filter(participante__email=email).first()
+
+	if not certificado:
+		messages.add_message(request, constants.WARNING, 'Certificado não encontrado')
+		return redirect(reverse('certificados_evento', kwargs={'id': evento.id}))
+	
+	return redirect(certificado.certificado.url)
--- type_event/urls.py
+++ type_event/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('usuarios/', include('usuarios.urls')),
-    path('eventos/', include('eventos.urls')),
+	path('eventos/', include('eventos.urls')),
+	path('cliente/', include('cliente.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
